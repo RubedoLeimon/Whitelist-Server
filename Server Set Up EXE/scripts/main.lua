f3784074c88e88e68a0a798e13538154a430986f
ninjaman255 <aaronclevette@gmail.com>
2022-03-07 00:57:52 +0000

    misc changes
    -added FG to backrooms
    -for some reason the whitelist path was changed again? had to set it back to the correct path
    -updating memory and BBS files

--- Server Set Up EXE/scripts/main.lua
+++ Server Set Up EXE/scripts/main.lua
@@ -108,7 +108,7 @@ function handle_player_avatar_change(player_id, details)
 end
 
 function handle_player_join(player_id)
-    Net.set_mod_whitelist_for_player(player,"/server/scripts/Whitelist.txt")
+    Net.set_mod_whitelist_for_player(player,"/server/assets/Whitelist.txt")
     for i,plugin in ipairs(plugins)do
         if plugin.handle_player_join then
             plugin.handle_player_join(player_id)
